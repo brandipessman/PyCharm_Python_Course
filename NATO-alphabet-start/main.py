bd0d0ca3e8834ebb41a1a2c132cf491c9cdc9c8b
brandipessman <bjpessman@gmail.com>
2023-08-31 12:32:43 -0500

    Learned about catching errors, raising exceptions, and json files. Modified the password generator

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -27,8 +27,16 @@ data_dict = {
     row.letter: row.code for (index, row) in data.iterrows()
 }
 
-word = input("Enter a word: ").upper()
-output_list = [data_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [data_dict[letter] for letter in word]
+    except KeyError:
+        print("Only enter letters in the alphabet.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
 
 
