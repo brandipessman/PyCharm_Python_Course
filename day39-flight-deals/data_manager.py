dd8d7951a3b70d00d9377d4d647becfc5d0a4059
brandipessman <bjpessman@gmail.com>
2023-09-20 10:01:53 -0500

    Built a flight search program using sheety and tequila

--- day39-flight-deals/data_manager.py
+++ day39-flight-deals/data_manager.py
@@ -1,20 +1,22 @@
 import requests
-from pprint import pprint
+from decouple import config
 
-SHEETY_URL = "URL"
-TOKEN = "TOKEN"
+SHEETY_URL = config('FLIGHT_SHEETY_URL')
+TOKEN = config('FLIGHT_SHEETY_TOKEN')
 
 sheety_header = {
     "Authorization": f"Bearer {TOKEN}"
 }
 
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+
+
     def __init__(self):
         self.destination_data = {}
 
+
     def get_destination_data(self):
-        response = requests.get(url=SHEETY_URL, headers=sheety_header)
+        response = requests.get(url=f"{SHEETY_URL}prices", headers=sheety_header)
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
@@ -27,8 +29,41 @@ class DataManager:
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_URL}/{city['id']}",
+                url=f"{SHEETY_URL}prices/{city['id']}",
                 json=new_data,
                 headers=sheety_header
             )
-            print(response.text)
+            #print(response.text)
+
+
+    def add_user(self):
+        print("Welcome to Brandi's Flight Club.\n We find the best flight deals and email you.")
+        first_name = input("What is your first name? ")
+        last_name = input("What is your last name? ")
+        created = False
+        while created == False:
+            email = input("What is your email? ")
+            email_confirm = input("Type your email again: ")
+            if email == email_confirm:
+                print("You're in the club!")
+                user_data = {
+                    "user": {
+                        "firstName": first_name,
+                        "lastName": last_name,
+                        "email": email
+                    }
+                }
+                headers = {
+                    "Authorization": f"Bearer {TOKEN}",
+                    "Content-Type": "application/json"
+                }
+                response = requests.post(
+                    url=f"{SHEETY_URL}users",
+                    json=user_data,
+                    headers=headers
+                )
+                response.raise_for_status()
+                print(response.text)
+                created = True
+            else:
+                print("Emails do not match. Please try again.")
--- day39-flight-deals/main.py
+++ day39-flight-deals/main.py
@@ -4,32 +4,52 @@ from datetime import *
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-print(sheet_data)
+#print(sheet_data)
 
 flight_search = FlightSearch()
 
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_code(city_names)
     data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+    # for row in sheet_data:
+    #     row["iataCode"] = flight_search.get_destination_code(row["city"])
+    # print(f"sheet_data:\n {sheet_data}")
+    #
+    # data_manager.destination_data = sheet_data
+    # data_manager.update_destination_codes()
 
 ORIGIN_CITY_IATA = "LON"
+
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data
+}
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-for destination in sheet_data:
+for destination_code in destinations:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
-        destination["iataCode"],
+        destination_code,
         from_time=tomorrow,
         to_time=six_month_from_today
     )
 
-    if flight.price < destination["lowestPrice"]:
+    if flight is None:
+        continue
+
+    if flight.price < destinations[destination_code]["price"]:
         message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
         print(message)
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+            print(message)
+
+data_manager.add_user()
