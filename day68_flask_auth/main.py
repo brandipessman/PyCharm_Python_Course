af0dc013b150a3588ee124ab713298bd5b6df964
brandipessman <bjpessman@gmail.com>
2024-06-26 15:29:38 -0500

    Learned how to authenticate users using hashing and salting

---
+++ day68_flask_auth/main.py
@@ -0,0 +1,129 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+# CREATE DATABASE
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# configure flask login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# create user_loader callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+# CREATE TABLE IN DB
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    # passing T/F if the user is authenticated
+    return render_template("index.html", logged_in = current_user.is_authenticated)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            #user exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        # hashing and salting
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method = 'pbkdf2:sha256',
+            salt_length = 8
+        )
+        new_user = User(
+            email = request.form.get('email'),
+            name = request.form.get('name'),
+            password = hash_and_salted_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        # login and authenticate user after adding details to database
+        login_user(new_user)
+
+        return render_template("secrets.html", name = request.form.get('name'))
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # find user by email
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        # email doesn't exit
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        # incorrect password
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
+    # pass T/F if the user is authenticated
+    return render_template("login.html", logged_in = current_user.is_authenticated)
+
+
+# only users that are logged in can access
+@app.route('/secrets')
+@login_required
+def secrets():
+    print(current_user.name)
+    # passing the name from current_user
+    return render_template("secrets.html", name = current_user.name, logged_in = True)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static', path = "files/cheat_sheet.pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
